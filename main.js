4f39e3e4700882545d12bb2d6a89379e53f30bb0
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-08-17 10:02:18 -0400

    done

--- main.js
+++ main.js
@@ -1,19 +1,62 @@
 /********************
  * HELPER FUNCTIONS *
  ********************/
+const getTodoName = function(todo){
+  return todo.text
+}
+
+const getPriority = function(todo){
+  return todo.priority
+}
+
+const isComplete = function(todo){
+  return todo.complete === true;
+}
+
+const isHighPriority = function(todo){
+  return todo.priority === 2
+}
 
 
 
+const namesAndPriority = function(todo) {
+  return getTodoName(todo) + '- ' + getPriority(todo);
+}
 
+const isNotComplete = function(todo){
+  return todo.complete !== true;
+}
 
-
-
-
-
+const isLowPriority = function(todo){
+  return todo.priority !== 2
+}
 
 /***********************
  * ITERATION FUNCTIONS *
  ***********************/
+const names = (todos)=> 
+todos.map((todo) => todo.text);
+
+const priorities = (todos)=> 
+todos.map((todo) => todo.priority);
+
+const namesAndPriorities = (todos)=>
+todos.map((todo)=> todo.namesAndPriority);
+
+const justNotComplete = (todos)=> 
+todos.filter(isNotComplete);
+
+const justComplete = (todos)=>
+todos.filter(isComplete);
+
+const priority2Only = (todos)=>
+todos.filter(isHighPriority);
+
+const priority1Only = (todos)=>
+todos.filter(isLowPriority);
+
+
+
 
 
 
